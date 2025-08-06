dd37a87510c30dec3fe469c1fdce5d74c4460e08
dozin <dojin0345@gmail.com>
2025-08-06 10:22:22 +0900

    refactor: useQote에 react-query 적용

--- features/quote/hooks/useQuote.ts
+++ features/quote/hooks/useQuote.ts
@@ -1,29 +1,16 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { Quote } from "../types";
+import { useQuery } from "@tanstack/react-query";
 import { getTodayQuote } from "../services/quoteService";
 
 export default function useQuote() {
-  const [quote, setQuote] = useState<Quote | null>(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState<Error | null>(null);
-
-  useEffect(() => {
-    const fetchQuote = async () => {
-      try {
-        setIsLoading(true);
-        const data = await getTodayQuote();
-        setQuote(data);
-      } catch (err) {
-        setError(
-          err instanceof Error ? err : new Error("Failed to fetch quote data")
-        );
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    fetchQuote();
-  }, []);
+  const {
+    data: quote,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["quote"],
+    queryFn: getTodayQuote,
+  });
   return { quote, isLoading, error };
 }
